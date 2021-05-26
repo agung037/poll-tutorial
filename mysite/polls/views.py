b57295f4d783d0bcb8e115d5c7ad0c9d740dc615
agung037 <agungk878@gmail.com>
2021-05-27 00:30:04 +0700

    create superadmin

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello world, you at the index")
+
+def result(request, question_id):
+    response = "you are looking at the result question %s"
